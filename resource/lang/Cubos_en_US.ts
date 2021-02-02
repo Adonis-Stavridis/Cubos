66da738dd426594a72aa07447ebe681e2acc958b
Adonis Stavridis <adonis.stavridis@gmail.com>
2021-02-02 18:50:37 +0100

    init

---
+++ resource/lang/Cubos_en_US.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Cubos_en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/Cubos_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
